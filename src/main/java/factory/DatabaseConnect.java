8ec98c0697361bde245765497e671274b70089ac
Renan Carvalho <renancarvalhorock@gmail.com>
2026-09-02 17:44:39 -0300

    implementado conexão mariaDB

--- src/main/java/factory/DatabaseConnect.java
+++ src/main/java/factory/DatabaseConnect.java
@@ -4,13 +4,22 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DatabaseConnect {
+    //conexão com postgresql
+    /*
     private static final String URL = "jdbc:postgresql://localhost:5432/agenda_telefonica";
     private static final String USER = "postgres";
     private static final String PASSWORD = "postgres";
+    */
+    //conexão com mariadb
+    private static final String URL = "jdbc:mariadb://localhost:3306/agenda_telefonica";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
 
     static {
         try {
-            Class.forName("org.postgresql.Driver");
+            //Class.forName("org.postgresql.Driver");
+            //mariaDb
+            Class.forName("org.mariadb.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             throw new RuntimeException("Driver do PostgreSQL não encontrado no classpath", e);
         }
